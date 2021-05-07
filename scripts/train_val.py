aab71402de554bb8b87f1932e24bc4b2b72c2a59
Ubuntu <ubuntu@furuyamaroon@gmail.com>
2021-05-07 14:53:28 +0800

    may 7 update

--- scripts/train_val.py
+++ scripts/train_val.py
@@ -31,28 +31,49 @@ with open('/home/ubuntu/Development/punctuation/data/val_set.pickle', 'rb') as f
 
 ids_no_punc_outer_adjusted_val, attention_mask_outer_adjusted_val, punc_mask_outer_adjusted_val, punc_mask_outer_val, tokenizer_val = data
 
+# class DistilBERT_Arch(nn.Module):
+#     def __init__(self, distilbert):
+#         super().__init__()
+#         self.distilbert = distilbert
+#         self.dropout = nn.Dropout(0.1)
+#         self.relu = nn.ReLU()
+#         self.fc1 = nn.Linear(768, 512)
+#         self.fc2 = nn.Linear(512, 4)
+#         self.softmax = nn.LogSoftmax(dim=1)
+#     def forward(self, input_ids, mask):        
+#         cls_hs = self.distilbert(input_ids, attention_mask=mask)[0]
+#         x = self.fc1(cls_hs)
+#         x = self.relu(x)
+#         x = self.dropout(x)
+#         x = self.fc2(x)
+#         x = self.softmax(x)
+#         return x
+    
 class DistilBERT_Arch(nn.Module):
     def __init__(self, distilbert):
         super().__init__()
         self.distilbert = distilbert
-        self.dropout = nn.Dropout(0.1)
+        self.dropout1 = nn.Dropout(0.2)
+        self.dropout2 = nn.Dropout(0.2)
         self.relu = nn.ReLU()
-        self.fc1 = nn.Linear(768, 512)
-        self.fc2 = nn.Linear(512, 4)
+        self.fc1 = nn.Linear(768, 1568)
+        self.fc2 = nn.Linear(1568, 4)
         self.softmax = nn.LogSoftmax(dim=1)
     def forward(self, input_ids, mask):        
         cls_hs = self.distilbert(input_ids, attention_mask=mask)[0]
-        x = self.fc1(cls_hs)
+        x = self.dropout1(cls_hs)
+        x = self.fc1(x)
+        x = self.dropout2(x)
         x = self.relu(x)
-        x = self.dropout(x)
         x = self.fc2(x)
         x = self.softmax(x)
         return x
     
+    
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-batch_size = 32
-epochs = 200
+batch_size = 8
+epochs = 60
 
 dataset_train = TensorDataset(ids_no_punc_outer_adjusted, attention_mask_outer_adjusted, punc_mask_outer_adjusted)
 dataloader_train = DataLoader(dataset_train, sampler=RandomSampler(dataset_train), batch_size=batch_size)
@@ -63,7 +84,7 @@ distilbert = DistilBertModel.from_pretrained("distilbert-base-uncased",
                                                     output_hidden_states=False)
 model = DistilBERT_Arch(distilbert)
 optimizer = AdamW(model.parameters(),
-                 lr=1e-5,
+                 lr=1e-4,
                  eps=1e-8)
 scheduler = get_linear_schedule_with_warmup(optimizer,
                                            num_warmup_steps=0,
@@ -75,8 +96,12 @@ for i, p in enumerate(punc_mask_outer):
         punc_cat = torch.cat((punc_cat, p), dim=0)
     else:
         punc_cat = p
-        
-class_weights = compute_class_weight('balanced', np.unique(punc_cat), punc_cat.numpy())
+
+############################################
+#giving class_weights a further adjustment
+############################################
+class_weights = compute_class_weight('balanced', np.unique(punc_cat), punc_cat.numpy()) * np.array([1, 2, 5, 2])
+
 #TODO: haven't considered attention_mask yet!!
 weights = torch.tensor(class_weights, dtype=torch.float)
 weights = weights.to(device)
@@ -106,7 +131,7 @@ os.makedirs(folder, exist_ok=True)
 
 for epoch in range(epochs):
 
-    if patience_cnt <= 4:
+    if patience_cnt <= 2:
 
         loss_total = 0
 
@@ -223,10 +248,10 @@ for epoch in range(epochs):
             pickle.dump(val_f1_weighted_list, f)
 
     else:
-        print(f'3rd consecutive degrades observed at epoch {epoch}. So the best is epoch {epoch-5}', flush=True)
+        print(f'3rd consecutive degrades observed at epoch {epoch}. So the best is epoch {epoch-3}', flush=True)
         break
 
-torch.save(param_list[-6], folder + '/distilbert_result.pt')
+torch.save(param_list[-4], folder + '/distilbert_result.pt')
         
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
